80d91da5080277302de5f1dd6acf42bf18e73ccc
Venky Kanniganti <dr.venkykanniganti@gmail.com>
2022-06-07 01:43:25 -0400

    WORKING AUTOMATION!

--- Atom Projects/WebForm.py
+++ Atom Projects/WebForm.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.select import Select
 
 options = webdriver.ChromeOptions()
+options.add_argument('--profile-directory=Default')
 service = ChromeService(executable_path='/Users/venky/Downloads/chromedriver')
 
 
@@ -14,7 +15,11 @@ driver.implicitly_wait(5)
 patronForm = driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdHxHZtzaKhjs_SGN6-eOQE5LX2h-dqlx8C5Z2cPhpyhHk09w/viewform")
 
 #Building Manager
-
+bmDropdown = driver.find_element(by = By.XPATH, value = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div[1]/div[1]')
+bmDropdown.click()
+bmAnswer = driver.find_element(by = By.XPATH, value = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[2]/div[31]')
+bmAnswer.click()
+bmAnswer.send_keys(Keys.ENTER)
 #TableTennis
 test = driver.find_element(by = By.XPATH, value ='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
 test.send_keys('0'+Keys.ENTER)
