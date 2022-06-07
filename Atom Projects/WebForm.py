ca7aae10e265656609a8f03e9cc1595d82b33670
Venky Kanniganti <dr.venkykanniganti@gmail.com>
2022-06-07 00:32:39 -0400

    Web Form Automation

---
+++ Atom Projects/WebForm.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.select import Select
+
+options = webdriver.ChromeOptions()
+service = ChromeService(executable_path='/Users/venky/Downloads/chromedriver')
+
+
+driver = webdriver.Chrome(service=service, options=options)
+driver.implicitly_wait(5)
+patronForm = driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdHxHZtzaKhjs_SGN6-eOQE5LX2h-dqlx8C5Z2cPhpyhHk09w/viewform")
+
+#Building Manager
+
+#TableTennis
+test = driver.find_element(by = By.XPATH, value ='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+test.send_keys('0'+Keys.ENTER)
