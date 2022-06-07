e5d5842adf0ae17a8007a34d28243c61f9e1f386
VenkyPC <dr.venkykanniganti@gmail.com>
2022-06-07 18:57:47 -0400

    Methods added

--- Atom Projects/WebForm.py
+++ Atom Projects/WebForm.py
@@ -15,11 +15,19 @@ driver.implicitly_wait(5)
 patronForm = driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdHxHZtzaKhjs_SGN6-eOQE5LX2h-dqlx8C5Z2cPhpyhHk09w/viewform")
 
 #Building Manager
-bmDropdown = driver.find_element(by = By.XPATH, value = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div[1]/div[1]')
-bmDropdown.click()
-bmAnswer = driver.find_element(by = By.XPATH, value = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[2]/div[31]')
-bmAnswer.click()
-bmAnswer.send_keys(Keys.ENTER)
+dropDown('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div[1]/div[1]','//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[2]/div[31]')
 #TableTennis
 test = driver.find_element(by = By.XPATH, value ='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
 test.send_keys('0'+Keys.ENTER)
+
+#method for dropdowns
+def dropDown(xpathDrop, xpathAns):
+    Drop = driver.find_element(by = By.XPATH, value = xpathDrop)
+    Drop.click()
+    Ans = driver.find_element(by = By.XPATH, value = xpathAns)
+    Ans.click()
+    Ans.send_keys(Keys.ENTER)
+#method for filling textbox
+def textFiller(xpath,text = '0')
+    textBox = driver.find_element(by = By.XPATH, value = xpath)
+    textBox.send_keys(text + Keys.ENTER)
